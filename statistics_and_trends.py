8b2859ee4feb0e7c849b9c715959285edae7c936
Srujan Gullapalli <jisoounniegullapalli@gmail.com>
2025-03-06 07:50:32 +0000

    Update statistics_and_trends.py

--- statistics_and_trends.py
+++ statistics_and_trends.py
@@ -18,7 +18,8 @@ def plot_relational_plot(df):
 
 
 def plot_categorical_plot(df):
-    """Generates a categorical plot (bar chart for suicide numbers by gender)."""
+    """Generates a categorical plot 
+    (bar chart for suicide numbers by gender)."""
     plt.figure(figsize=(8, 5))
     sns.barplot(x='sex', y='suicides_no', data=df, estimator=sum)
     plt.title('Total Suicides by Gender')
